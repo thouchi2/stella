243250ce8981ebe587a7e3a5211020f1e9088ea6
Troy Houchin <TroyHouchin@wirelessprv-10-194-232-151.near.illinois.edu>
2018-07-05 18:03:31 -0500

    fixed travis

--- install-linux-deps.sh
+++ install-linux-deps.sh
@@ -8,3 +8,11 @@ pushd $PETSC_DIR
 ./configure --with-debug=yes --with-shared-libraries=1 --download-hypre --download-mpich --download-f2cblaslapack=1
 make
 popd
+
+wget -q --no-check-certificate http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz
+tar -xzf mpich-3.2.tar.gz
+cd mpich-3.2
+mkdir build && cd build
+../configure CC=$PRK_CC CXX=$PRK_CXX --disable-fortran --prefix=$TRAVIS_ROOT/mpich
+make -j4
+make install
