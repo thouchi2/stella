a104798873b86d166026238ac00190b71d34ee42
Troy Houchin <TroyHouchin@wirelessprv-10-194-232-151.near.illinois.edu>
2018-07-05 15:08:15 -0500

    fixed travis

--- install-linux-deps.sh
+++ install-linux-deps.sh
@@ -1,10 +1,10 @@
 sudo apt-get update -qq
-sudo apt-get install -y cmake3 mpich doxygen git
+sudo apt-get install -y cmake gcc mpich doxygen git
 export TMPDIR=/tmp
 
 # Go get PETSc and build it.
 git clone https://bitbucket.org/petsc/petsc petsc
 pushd $PETSC_DIR
-./configure --with-mpi=1 --with-debug=$DEBUG --with-shared-libraries=1 --download-hypre
+./configure --with-mpi=1 --with-debug=$DEBUG --with-shared-libraries=1 --download-hypre --downloadmpich
 make
 popd
