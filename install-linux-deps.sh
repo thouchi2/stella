d8bb49821ff38a0e2a647ba97fdbe09e18e25de4
Troy Houchin <TroyHouchin@wirelessprv-10-194-232-151.near.illinois.edu>
2018-07-05 16:53:29 -0500

    fixed travis

--- install-linux-deps.sh
+++ install-linux-deps.sh
@@ -5,6 +5,6 @@ export TMPDIR=/tmp
 # Go get PETSc and build it.
 git clone https://bitbucket.org/petsc/petsc petsc
 pushd $PETSC_DIR
-./configure --with-mpi=1 --with-debug=$DEBUG --with-shared-libraries=1 --download-hypre --downloadmpich
+./configure --with-debug=yes --with-shared-libraries=1 --download-hypre --download-mpich
 make
 popd
