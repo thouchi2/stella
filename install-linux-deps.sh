df584b6c9b3f4c9af8f762093f1173cca92f9a1d
Troy Houchin <TroyHouchin@wirelessprv-10-194-232-151.near.illinois.edu>
2018-07-05 15:50:01 -0500

    fixed travis

--- install-linux-deps.sh
+++ install-linux-deps.sh
@@ -1,5 +1,5 @@
 sudo apt-get update -qq
-sudo apt-get install -y cmake gcc mpich doxygen git
+sudo apt-get install -y cmake gcc gfortran mpich doxygen git
 export TMPDIR=/tmp
 
 # Go get PETSc and build it.
