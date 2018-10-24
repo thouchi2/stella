7204b77aa8f4106dee0a64e2e658c6a005d7bf45
Andrew Reisner <areisne2@illinois.edu>
2018-10-24 11:06:48 -0500

    Use PetscUnlikely for checking error codes.

--- src/stella_cedar.h
+++ src/stella_cedar.h
@@ -1,7 +1,7 @@
 #ifndef CEDAR_STELLA_H
 #define CEDAR_STELLA_H
 
-#define chkerr(cedar_err) do{if (cedar_err) { SETERRQ1(PETSC_COMM_WORLD, PETSC_ERR_LIB, "Cedar error code: %d", cedar_err); }} while(0)
+#define chkerr(cedar_err) do{if (PetscUnlikely(cedar_err)) { SETERRQ1(PETSC_COMM_WORLD, PETSC_ERR_LIB, "Cedar error code: %d", cedar_err); }} while(0)
 
 #ifdef WITH_CEDAR
 #include <cedar/capi.h>
